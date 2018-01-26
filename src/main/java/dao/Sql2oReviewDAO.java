3b981989d1e2512a4e91de4cb437a344d4582249
Ross Fletcher <rtbfletch@outlook.com>
2018-01-26 08:43:52 -0800

    have passing test for delete review by id

--- src/main/java/dao/Sql2oReviewDAO.java
+++ src/main/java/dao/Sql2oReviewDAO.java
@@ -48,7 +48,14 @@ public class Sql2oReviewDAO implements ReviewDAO {
 
     @Override
     public void deleteById(int id) {
-
+        String sql = "DELETE from reviews WHERE  id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
 
